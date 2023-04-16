71a0264b895c106c474f7d8644b8ff0019fa3305
unknown <matroskin7777@gmail.com>
2023-04-16 15:29:05 +0300

    Fixed 5.1 + square

--- Home_task_5/EX5.1/EX5.1/FenceCounter.cs
+++ Home_task_5/EX5.1/EX5.1/FenceCounter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,24 +15,17 @@ namespace EX5._1
         public FenceCounter(List<Point> points)
         {
             _points = points;
+            _points.Sort(new PointComparer());
         }
 
-        public int FindShortestFence()
+        public double FindShortestFence()
         {
-            int length = 0;
-            for(int i = 0; i < _points.Count - 1; i++)
+            List<Point> hull = Jarvis();
+            double length = 0;
+            for(int i = 0; i < hull.Count - 1; i++)
             {
-                int currLength = int.MaxValue;
-                for (int j = i + 1; j < _points.Count; j++) {
-                    int temp = (int)Math.Sqrt(Math.Pow(_points[j].X - _points[i].X, 2) + Math.Pow(_points[j].Y - _points[i].Y, 2));
-                    if (temp < currLength) {
-                        currLength = temp;
-                        (_points[i + 1], _points[j]) = (_points[j], _points[i + 1]);
-                    }
-                }
-                length += currLength;
+                length += Math.Sqrt(Math.Pow(hull[i].X - hull[i + 1].X, 2) + Math.Pow(hull[i].Y - hull[i + 1].Y, 2));
             }
-            length += (int)Math.Sqrt(Math.Pow(_points[_points.Count - 1].X - _points[0].X, 2) + Math.Pow(_points[_points.Count - 1].Y - _points[0].Y, 2));
             return length;
         }
 
@@ -45,17 +39,72 @@ namespace EX5._1
             return x.FindShortestFence() < y.FindShortestFence();
         }
 
-        //public double FindSquare()
-        //{
-        //    double square = 0;
-        //    for(int i = 1; i < _points.Count; i++)
-        //    {
-        //        if (_points[i].X != _points[i - 1].X && _points[i].Y != _points[i - 1].Y)
-        //        {
-        //            square += (MathF.Abs(_points[i].X - _points[i - 1].X) * MathF.Abs(_points[i].Y - _points[i - 1].Y)) / 2;
-        //        }
-        //    }
-        //    return square;
-        //}
+        private List<Point> Jarvis()
+        {
+            List<Point> points = new List<Point>();
+            points.Add(_points[0]);
+            Point current = _points.Where(point => point.X == _points.Min(x => x.X)).First();
+            Point endPoint;
+            do
+            {
+                points.Add(current);
+                endPoint = _points[0];
+
+                for(int i = 1; i < _points.Count; i++)
+                {
+                    if(current == endPoint || Position(current, endPoint, _points[i]) == -1)
+                    {
+                        endPoint = _points[i];
+                    }
+                }
+                current = endPoint;
+            }
+            while (current != _points[0]);
+            points = points.Distinct().ToList();
+            points.Add(_points[0]);
+            return points;
+        }
+
+        private int Position(Point p1, Point p2, Point p)
+        {
+            int pos = (p2.X - p1.X) * (p.Y - p1.Y) - (p.X - p1.X) * (p2.Y - p1.Y);
+            if(pos < 0)
+            {
+                return 1;
+            }
+            if(pos > 0)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+
+
+        public double FindSquare()
+        {
+            List<Point> points = Jarvis();
+            List<List<Point>> trianges = new List<List<Point>>();
+            for(int i = 0; i < points.Count; i ++)
+            {
+                if (i % 3 == 0)
+                {
+                    trianges.Add(new List<Point>());
+                }
+                if(i % 3 == 0 && i != 0)
+                    trianges[trianges.Count - 1].Add(points[i - 1]);
+                trianges[trianges.Count - 1].Add(points[i]);
+            }
+            double square = 0;
+            for(int i = 0; i < trianges.Count; i++)
+            {
+                double first = Math.Sqrt(Math.Pow(trianges[i][0].X - trianges[i][1].X, 2) + Math.Pow(trianges[i][0].Y - trianges[i][1].Y, 2));
+                double second = Math.Sqrt(Math.Pow(trianges[i][0].X - trianges[i][2].X, 2) + Math.Pow(trianges[i][0].Y - trianges[i][2].Y, 2));
+                double third = Math.Sqrt(Math.Pow(trianges[i][1].X - trianges[i][2].X, 2) + Math.Pow(trianges[i][1].Y - trianges[i][2].Y, 2));
+                double halfPerimeter = (first + second + third) / 2;
+                square += Math.Sqrt(halfPerimeter * (halfPerimeter - first) * (halfPerimeter - second) * (halfPerimeter - third));
+            }
+            return square;
+        }
     }
 }
--- Home_task_5/EX5.1/EX5.1/Program.cs
+++ Home_task_5/EX5.1/EX5.1/Program.cs
@@ -7,11 +7,16 @@ namespace EX5._1
         static void Main(string[] args)
         {
             FenceCounter fenceCounter = new FenceCounter(new List<Point>() { new Point(3, 4), new Point(4, 3), new Point(3, 2), new Point(4, 1)});
-            FenceCounter fenceCounter1 = new FenceCounter(new List<Point>() { new Point(3, 5), new Point(6, 2), new Point(8, 2), new Point(4, 9), new Point(3, 7), new Point(0, 0) });
+            FenceCounter fenceCounter1 = new FenceCounter(new List<Point>() { new Point(3, 5), new Point(6, 2), new Point(8, 2), new Point(4, 9), new Point(3, 7), new Point(0, 0), new Point(0, 10) });
+            FenceCounter fenceCounter2 = new FenceCounter(new List<Point>() { new Point(0, 0), new Point(0, 1) });
             Console.WriteLine(fenceCounter.FindShortestFence());
             Console.WriteLine(fenceCounter1.FindShortestFence());
+            Console.WriteLine(fenceCounter2.FindShortestFence());
             Console.WriteLine(fenceCounter > fenceCounter1);
             Console.WriteLine(fenceCounter < fenceCounter1);
+            Console.WriteLine(fenceCounter.FindSquare());
+            Console.WriteLine(fenceCounter1.FindSquare());
+            Console.WriteLine(fenceCounter2.FindSquare());
         }
     }
 }
