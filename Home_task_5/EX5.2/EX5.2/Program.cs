ca5584e822fa71011d5e910803855a2e3c9fcfc3
Raven356 <71709087+Raven356@users.noreply.github.com>
2023-04-17 21:12:46 +0300

    removed comments 5.2

--- Home_task_5/EX5.2/EX5.2/Program.cs
+++ Home_task_5/EX5.2/EX5.2/Program.cs
@@ -6,24 +6,6 @@
         {
             Menu menu = new Menu();
             menu.StartMenu();
-            //SuperMarketGenerator superMarketGenerator = new SuperMarketGenerator();
-            //SuperMarket superMarket = superMarketGenerator.Generate(2);
-            //List<string> goodsNames = superMarketGenerator.GoodsNames;
-            //Wrapper wrapper = new Wrapper();
-            //wrapper.Wrap(superMarket);
-            //Console.WriteLine("Goods names:");
-            //foreach (var x in goodsNames)
-            //{
-            //    Console.WriteLine(x);
-            //}
-            //Console.WriteLine("Enter goods which you want to find:");
-            //string answer = Console.ReadLine();
-            //PathFinder pathFinder = new PathFinder();
-            //string path = pathFinder.FindPath(wrapper.Box, answer);
-            //if(path != null)
-            //    Console.WriteLine("\n" + path);
-            //else
-            //    Console.WriteLine("\nGoods not found!");
         }
     }
 }
