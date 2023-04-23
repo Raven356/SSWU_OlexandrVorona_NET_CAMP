4a484c1c65f6536165fb7b4f3bc37539a86d9e65
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-24 02:17:45 +0300

    Update Program.cs

--- Home_task_4/EX4.1/EX4.1/Program.cs
+++ Home_task_4/EX4.1/EX4.1/Program.cs
@@ -8,6 +8,7 @@
             List<string> text = spliter.SplitText("djksdjfhjakshjkadasdsad.\nasdaalsjdasa ask(ldjs)a dsakld\n" +
                 "lsadas! asda lasdlassda\n? kasokdl [asdkasd wlelq;we] lq\nkasdkasdk!\nasdas asdasd ( askdaskd ) asdasld!\nkasdasdlas \nsakasd {sadlasld} lasda?");
             StatementFinder statementFinder = new StatementFinder();
+            // ви задачу об'єднання кусків винесли на клієнта, а це не добре.
             var result = statementFinder.FindStatements(text);
             foreach (var x in result)
             {
