81e8f0fb43ee59906aef6b2b7bc7fa5d2b69a228
unknown <matroskin7777@gmail.com>
2023-04-29 16:42:37 +0300

    Added another variant for ex6.3

--- Home_task_6/EX6.3/EX6.3/Program.cs
+++ Home_task_6/EX6.3/EX6.3/Program.cs
@@ -5,7 +5,13 @@
         static void Main(string[] args)
         {
             UniqueWordsFinder uniqueWordsFinder = new UniqueWordsFinder();
-            List<string> words = uniqueWordsFinder.FindUnique("cat dog pig askadskj cat, leaf dog!").ToList();
+            List<string> words = uniqueWordsFinder.FindWithoutDuplicates("cat dog pig askadskj cat, leaf dog!").ToList();
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine();
+            words = uniqueWordsFinder.FindUnique("cat dog pig askadskj cat, leaf dog!").ToList();
             foreach (string word in words)
             {
                 Console.WriteLine(word);
--- Home_task_6/EX6.3/EX6.3/UniqueWordsFinder.cs
+++ Home_task_6/EX6.3/EX6.3/UniqueWordsFinder.cs
@@ -8,7 +8,8 @@ namespace EX6._3
 {
     internal class UniqueWordsFinder
     {
-        public IEnumerable<string> FindUnique(string text)
+        //return words that doesn't have duplicates
+        public IEnumerable<string> FindWithoutDuplicates(string text)
         {
             string[] words = text.Split(' ', ',', '.', '!', '?', ';', ':');
             foreach (string word in words)
@@ -16,6 +17,31 @@ namespace EX6._3
                 if (words.Count(x => x.Equals(word)) == 1 && !word.Equals(""))
                 {
                     yield return word;
+                }
+            }
+        }
+
+        //return only unique words
+        public IEnumerable<string> FindUnique(string text)
+        {
+            string[] words = text.Split(' ', ',', '.', '!', '?', ';', ':');
+            for(int i = 0; i < words.Length; i++)
+            {
+                bool returned = false;
+                if (!words[i].Equals(""))
+                {
+                    for (int j = i - 1; j >= 0; j--)
+                    {
+                        if (words[i].Equals(words[j]))
+                        {
+                            returned = true;
+                            break;
+                        }
+                    }
+                    if (!returned)
+                    {
+                        yield return words[i];
+                    }
                 }
             }
         }
